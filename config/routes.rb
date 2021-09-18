1197ba786bd229ddc628768942e0ce1f3139dcca
carbohydrate0 <e6a201052d@edu.teu.ac.jp>
2021-09-18 21:08:09 +0000

    [Add]bootstrap導入前保存

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/show'
+  devise_for :users
+  root to: 'homes#top'
+  resources :book, only: [:new, :create, :index, :show, :destroy]
 end
